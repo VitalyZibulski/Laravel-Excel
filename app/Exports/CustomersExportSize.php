8a2aa4f733e24ea0cb3dbab5f894ff0883437c8f
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2020-07-18 23:58:07 +0300

    add size manually

--- app/Exports/CustomersExportSize.php
+++ app/Exports/CustomersExportSize.php
@@ -5,8 +5,10 @@ namespace App\Exports;
 use App\Customer;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class CustomersExportSize implements FromCollection, ShouldAutoSize
+class CustomersExportSize implements FromCollection, ShouldAutoSize, WithEvents
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -14,5 +16,20 @@ class CustomersExportSize implements FromCollection, ShouldAutoSize
     public function collection()
     {
         return Customer::all();
+    }
+
+    /**
+     * @return array
+     */
+    public function registerEvents(): array
+    {
+        return [
+          AfterSheet::class => function(AfterSheet $event) {
+            $event->sheet->getDelegate()->getRowDimension(1)->setRowHeight(20);
+
+            $event->sheet->getDelegate()->getStyle('A1:D4')
+                ->getAlignment()->setWrapText(true);
+          }
+        ];
     }
 }
